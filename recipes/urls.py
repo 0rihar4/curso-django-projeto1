135b5410a2ae9b675d6e2521eb32bcfc8dbc36fc
João Vitor <joaovitoralvesmorais01@gmail.com>
2023-02-04 16:12:57 -0300

    Removido contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,5 @@ from recipes import views
 
 urlpatterns = [
     path('', views.home),
-    path('sobre/', views.sobre),
-    path('contato/', views.contato),
+
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
-
-
-def sobre(request):
-    # request -> solicitação do cliente
-    # response -> respota do servidor
-    return render(request, 'recipes/sobre.html', context={
-        'name': 'João Vitor',
-    })
 
 
 def home(request):
@@ -16,9 +7,3 @@ def home(request):
     # response -> respota do servidor
     return render(request, 'recipes/home.html', context={
         'name': 'João Vitor'})
-
-
-def contato(request):
-    # request -> solicitação do cliente
-    # response -> respota do servidor
-    return render(request, 'me-apague/apague.html')
