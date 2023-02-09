04feba0743865023eda78a7b32dd92e7a6d0b905
João Vitor <joaovitoralvesmorais01@gmail.com>
2023-02-09 17:04:50 -0300

    Finalização da primeira parte do site

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+# from utils.recipes.factory import make_recipe
+from .models import Recipe
+
 # Create your views here.
 
 
 def home(request):
     # request -> solicitação do cliente
     # response -> respota do servidor
+    recipes = Recipe.objects.all().filter(
+        is_published=True,
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'João Vitor'})
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    # request -> solicitação do cliente
+    # response -> respota do servidor
+    # é possivel passar uma query pronta dentro do get_list_or_404
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+    # category_name = getattr(recipes.first(), 'category', None)
+    # recipes get_list_or_404(
+    #     # Model escolhido
+    #     Recipe,
+    #     # Filtros para a query
+    #     category__id=category_id,is_published=True,
+    #     )
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name}',
+    })
+
+
+def recipe(request, id):
+    # recipe = Recipe.objects.filter(
+    #     pk=id,
+    #     is_published=True,
+    # ).order_by('-id').first()
+    recipe = get_object_or_404(Recipe, pk=id,
+                               is_published=True,)
+    return render(request, 'recipes/pages/single-recipe.html', context={
+        'recipe': recipe,
+        'is_detail_page': True})
