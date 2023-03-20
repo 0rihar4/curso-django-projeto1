2557fd688010f5774ccc2d308f4dabdc973c056e
João Vitor <joaovitoralvesmorais01@gmail.com>
2023-03-20 20:09:35 -0300

    Configurando Allowed Host e o DEBUG

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')  # noqa:E501
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS: list = []
+ALLOWED_HOSTS: list = ['https://www.acouguesantana.com/']
 
 
 # Application definition
